ed85bbc036fa05eb487f1a4f00fedfccab1b2d8e
atharvapcat@gmail.com <atharvapcat@gmail.com>
2025-03-01 15:35:25 -0700

    Added async voice generation

--- elevenapi.py
+++ elevenapi.py
@@ -1,21 +1,45 @@
 from dotenv import load_dotenv
-from elevenlabs.client import ElevenLabs
-from elevenlabs import play
 import os
+from elevenlabs.client import ElevenLabs
+from elevenlabs.client import AsyncElevenLabs
+from elevenlabs import play
 
-load_dotenv()
 
 class elevenlabs:
 
     def __init__(self):
+        load_dotenv()
         self.client = ElevenLabs(api_key=os.getenv("ELEVENLABS_API_KEY"))
+        self.async_client = AsyncElevenLabs(api_key=os.getenv("ELEVENLABS_API_KEY"))
         self.output_format = "mp3_44100_128"
-        self.model_id="eleven_multilingual_v2",
-        self.voice_id="JBFqnCBsd6RMkjVDRZzb",
-
+        self.model_id="eleven_multilingual_v2"
+        self.voice_id="JBFqnCBsd6RMkjVDRZzb"
 
     def text_to_speech_audio(self, text):
+        """
+        Returns:
+            Audio object of output format mp3_44100_128
+        """
+
         audio = self.client.text_to_speech.convert(
+            text=text,
+            voice_id=self.voice_id,
+            model_id=self.model_id,
+            output_format=self.output_format,
+        )
+
+        return audio
+    
+
+    async def text_to_speech_audio(self, text):
+        """
+        Async compatible tts generation 
+
+        Returns:
+            Audio object of output format mp3_44100_128
+        """
+
+        audio = self.async_client.text_to_speech.convert(
             text=text,
             voice_id=self.voice_id,
             model_id=self.model_id,
